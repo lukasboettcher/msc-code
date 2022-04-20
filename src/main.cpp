d27517ca17bd94b7ebdd08f072e5c5fb146b159d
Lukas Boettcher <1340215+lukasboettcher@users.noreply.github.com>
2022-04-20 21:46:26 +0200

    added simple driver program parses pag graph and writes meta + edge info

---
+++ src/main.cpp
@@ -0,0 +1,135 @@
+#include "SVF-FE/LLVMUtil.h"
+#include "Graphs/SVFG.h"
+#include "WPA/Andersen.h"
+#include "SVF-FE/SVFIRBuilder.h"
+#include "Util/Options.h"
+
+#include "Graphs/IRGraph.h"
+
+using namespace llvm;
+using namespace std;
+using namespace SVF;
+
+static llvm::cl::opt<std::string> InputFilename(cl::Positional,
+                                                llvm::cl::desc("<input bitcode>"), llvm::cl::init("-"));
+
+/*!
+ * An example to query alias results of two LLVM values
+ */
+SVF::AliasResult aliasQuery(PointerAnalysis *pta, Value *v1, Value *v2)
+{
+    return pta->alias(v1, v2);
+}
+
+/*!
+ * An example to print points-to set of an LLVM value
+ */
+std::string printPts(PointerAnalysis *pta, Value *val)
+{
+
+    std::string str;
+    raw_string_ostream rawstr(str);
+
+    NodeID pNodeId = pta->getPAG()->getValueNode(val);
+    const PointsTo &pts = pta->getPts(pNodeId);
+    for (PointsTo::iterator ii = pts.begin(), ie = pts.end();
+         ii != ie; ii++)
+    {
+        rawstr << " " << *ii << " ";
+        PAGNode *targetObj = pta->getPAG()->getGNode(*ii);
+        if (targetObj->hasValue())
+        {
+            rawstr << "(" << *targetObj->getValue() << ")\t ";
+        }
+    }
+
+    return rawstr.str();
+}
+
+// // statement types Addr, Copy, Store, Load, Call, Ret, Gep, Phi, Select, Cmp, BinaryOp, UnaryOp, Branch, ThreadFork, ThreadJoin
+
+std::string getStmtStr(SVF::GenericEdge<SVF::SVFVar>::GEdgeKind type)
+{
+    switch (type)
+    {
+    case SVFStmt::Addr:
+        return "Addr";
+    case SVFStmt::Copy:
+        return "Copy";
+    case SVFStmt::Store:
+        return "Store";
+    case SVFStmt::Load:
+        return "Load";
+    case SVFStmt::Call:
+        return "Call";
+    case SVFStmt::Ret:
+        return "Ret";
+    case SVFStmt::Gep:
+        return "Gep";
+    case SVFStmt::Phi:
+        return "Phi";
+    case SVFStmt::Select:
+        return "Select";
+    case SVFStmt::Cmp:
+        return "Cmp";
+    case SVFStmt::BinaryOp:
+        return "BinaryOp";
+    case SVFStmt::UnaryOp:
+        return "UnaryOp";
+    case SVFStmt::Branch:
+        return "Branch";
+    case SVFStmt::ThreadFork:
+        return "ThreadFork";
+    case SVFStmt::ThreadJoin:
+        return "ThreadJoin";
+    default:
+        return "invalid";
+    }
+}
+
+int main(int argc, char **argv)
+{
+
+    int arg_num = 0;
+    char **arg_value = new char *[argc];
+    std::vector<std::string> moduleNameVec;
+    SVFUtil::processArguments(argc, argv, arg_num, arg_value, moduleNameVec);
+    cl::ParseCommandLineOptions(arg_num, arg_value,
+                                "Whole Program Points-to Analysis\n");
+
+    if (Options::WriteAnder == "ir_annotator")
+    {
+        LLVMModuleSet::getLLVMModuleSet()->preProcessBCs(moduleNameVec);
+    }
+
+    SVFModule *svfModule = LLVMModuleSet::getLLVMModuleSet()->buildSVFModule(moduleNameVec);
+    svfModule->buildSymbolTableInfo();
+
+    /// Build Program Assignment Graph (SVFIR)
+    SVFIRBuilder builder;
+    SVFIR *pag = builder.build(svfModule);
+    // pag->dump("graph");
+
+    ofstream edge_stream, meta_stream;
+    edge_stream.open("edges.txt");
+    meta_stream.open("meta.txt");
+
+    meta_stream << "total nodes: " << pag->getPAGNodeNum() << "\ttotal edges: " << pag->getPAGEdgeNum() << endl;
+
+    for (auto &entry : *pag)
+    {
+        // push desc into meta.txt
+        meta_stream << entry.second->toString() << endl;
+
+        for (auto x : entry.second->getOutEdges())
+        {
+            // push src dst typ into edges.txt
+            edge_stream << x->getSrcID() << "\t->\t" << x->getDstID() << "\t" << x->getEdgeKind() << endl;
+        }
+    }
+
+    SVFIR::releaseSVFIR();
+    SVF::LLVMModuleSet::releaseLLVMModuleSet();
+    llvm::llvm_shutdown();
+    return 0;
+}
