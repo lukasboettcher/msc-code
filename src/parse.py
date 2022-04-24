afa54e63acf89c4e2bb8bfe61bdd8c6fd3650ddc
Lukas Boettcher <1340215+lukasboettcher@users.noreply.github.com>
2022-04-24 22:32:56 +0200

    switch over to the new graph class this allows better operations on the graph for example add transitive edges for all (d, -d) edges

--- src/parse.py
+++ src/parse.py
@@ -105,56 +105,45 @@ class LLVM2GRAPH:
     def __init__(self, GV=False):
         self.graphPath = sys.argv[1]
         self.outPath = self.graphPath + '.modified'
-        self.map = {}
-        self.edges = []
-        self.counter = 0
-
+        self.graph = Graph()
         self.LOAD = "3"
         self.STORE = "2"
         self.GV = GV
-
-        self.inFile = open(self.graphPath)
-        self.outFile = open(self.outPath, 'w')
-        self.dot = graphviz.Digraph()
-
-    def insert_or_get(self, name: str):
-        if not name in self.map:
-            self.map[name] = self.counter
-            self.counter +=1
-            return self.map[name]
-        else:
-            return self.map[name]
     
-    def write_to_file(self, src, dst, type):
-        
-        if type == self.STORE:
-            self.outFile.write(f"{self.insert_or_get('&'+src)}\t{self.insert_or_get(src)}\td\t\n")
-            self.outFile.write(f"{self.insert_or_get(src)}\t{self.insert_or_get('&'+src)}\t-d\t\n")
-            self.outFile.write(f"{self.insert_or_get('&'+src)}\t{dst}\ta\t\n")
-        elif type == self.LOAD:
-            self.outFile.write(f"{self.insert_or_get(src)}\t{self.insert_or_get(dst)}\td\t\n")
-            self.outFile.write(f"{self.insert_or_get(dst)}\t{self.insert_or_get(src)}\t-d\t\n")
+    def write_to_file(self, outPath: str):
+        with open(outPath, 'w') as outFile:
+            for e in self.graph.edges:
+                outFile.write(f'{e.src}\t{e.dst}\t{e.type}\n')
 
-    def write_to_graphviz(self, src, dst, type):
+    def add_to_graph(self, src, dst, type):
+
+        src_id = self.graph.add_or_get_node(src)
+        src_addr_id = self.graph.add_or_get_node('&'+src)
+        dst_id = self.graph.add_or_get_node(dst)
+
         if type == self.STORE:
-            self.dot.edge(src, dst, "STORE")
+            self.graph.add_edge(src_addr_id, src_id, 'd')
+            self.graph.add_edge(src_id, src_addr_id, '-d')
+            self.graph.add_edge(src_addr_id, dst_id, 'a')
         elif type == self.LOAD:
-            self.dot.edge(src, dst, "LOAD")
+            self.graph.add_edge(src_id, dst_id, 'd')
+            self.graph.add_edge(dst_id, src_id, '-d')
 
     def run(self):
-        lines = self.inFile.readlines()
-        for line in lines:
-            self.edges.append(line.split())
-        for e in [e for e in self.edges if e[3] in ['2','3']]:
-            src, _, dst, type = e
-            self.write_to_file(src, dst, type)
-            if self.GV:
-                self.write_to_graphviz(src, dst, type)
-        self.dot.render()
-        # for e in [e for e in self.edges if e[3] in ['2','3']]:
-        #     print(f"{e}   {self.insert_or_get(e[0])} -> {self.insert_or_get(e[2])}")
-        for k, v in self.map.items():
-            print(f"{v} - {k}")
+        with open(self.graphPath) as graphFile:
+            # add d, -d and a edges to graph for all STORE and LOAD operations
+            for src, _, dst, type in [l.split() for l in graphFile.readlines()]:
+                if type in ['2','3']:
+                    self.add_to_graph(src, dst, type)
+
+        for alias_nodes in filter(None,[[edge.dst for edge in v.get_out_edges() if edge.type == '-d'] for k,v in self.graph.id2node.items()]):
+            for a,b in combinations(alias_nodes,2):
+                self.graph.clone_out_edges(a,b)
+
+        self.graph.print()
+        if self.GV:
+            self.graph.print_gv()
+        self.write_to_file(self.outPath)
 
 
 main = LLVM2GRAPH(GV=True)
