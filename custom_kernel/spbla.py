87dca9f3581a62047441205fdc8c2d0a54bcd394
Lukas Boettcher <1340215+lukasboettcher@users.noreply.github.com>
2022-05-18 02:18:16 +0200

    initialize to -1 in transitive closure sometimes the t matrix starts empty

--- custom_kernel/spbla.py
+++ custom_kernel/spbla.py
@@ -2,18 +2,7 @@ import pyspbla as sp
 import sys
 from typing import List, Dict, Any, Tuple, Set, Union
 
-def transitive_closure(a: sp.Matrix):
-    """
-    Evaluates transitive closure for the provided
-    adjacency matrix of the graph.
-
-    :param a: Adjacency matrix of the graph
-    :return: The transitive closure adjacency matrix
-    """
-
-    t = a.dup()  # Duplicate matrix where to store result
-    total = 0  # Current number of values
-
+    total = -1
     while total != t.nvals:
         total = t.nvals
         t.mxm(t, out=t, accumulate=True)  # t += t * t
