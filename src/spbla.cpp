d198a67344498dfefd721dad6088add9ed67d266
Lukas Boettcher <1340215+lukasboettcher@users.noreply.github.com>
2022-05-23 03:36:51 +0200

    created function to parse rules from stream save all three CNF types separately one map for terminals, one for eps edges and one for the production rules

--- src/spbla.cpp
+++ src/spbla.cpp
@@ -1,4 +1,32 @@
 #include <spbla/spbla.h>
+void parse_rules(
+    istream *s,
+    unordered_set<string> &epsilon_nonterminals,
+    unordered_map<string, unordered_set<string>> &terminal_to_nonterminals,
+    vector<pair<string, pair<string, string>>> &rules,
+    unordered_set<string> &symbols)
+{
+    string symbol, line;
+    size_t k = 0;
+    while (getline(*s, line))
+    {
+        vector<string> l_syms;
+        istringstream linestream(line);
+        while (linestream >> symbol)
+            l_syms.push_back(symbol);
+        symbols.insert(l_syms[0]);
+        if (l_syms.size() == 1)
+            epsilon_nonterminals.insert(l_syms[0]);
+        else if (l_syms.size() == 2)
+        {
+            terminal_to_nonterminals[l_syms[1]].insert(l_syms[0]);
+        }
+        else if (l_syms.size() == 3)
+        {
+            rules.push_back({l_syms[0], {l_syms[1], l_syms[2]}});
+        }
+    }
+}
 
 int main(int argc, char const *argv[])
 {
