3eb776646056513a914f969cb5299e7c85bb2617
Lukas Boettcher <1340215+lukasboettcher@users.noreply.github.com>
2022-06-18 00:23:20 +0200

    added struct for adj matrix type

--- include/mmxcflr.h
+++ include/mmxcflr.h
@@ -14,12 +14,22 @@ inline void spblaAssert(spbla_Status code, const char *file, int line, bool abor
       if (abort) exit(code);
    }
 }
+
+struct AdjMatrix {
+    spbla_Index *rows;
+    spbla_Index *cols;
+    spbla_Index nvals;
+};
+
 typedef std::map<std::string, spbla_Matrix> MatrixMap;
 typedef std::unordered_set<std::string> SymbolSet;
 typedef std::pair<std::string, std::pair<std::string, std::string>> Rule;
 typedef std::vector<Rule> Rules;
 typedef std::vector<spbla_Index> spbla_vec_t;
 typedef std::unordered_map<std::string, std::pair<spbla_vec_t, spbla_vec_t>> Edges;
+typedef struct AdjMatrix AdjMatrix;
+
+
 
 
 #endif
