dd13626ec6ad459100a591ac6b213d07531fc59e
Lukas Boettcher <1340215+lukasboettcher@users.noreply.github.com>
2022-06-17 22:13:10 +0200

    add shortened types to mmxcflr header

--- include/mmxcflr.h
+++ include/mmxcflr.h
@@ -4,4 +4,11 @@
 
 #include "spbla/spbla.h"
 #include <bits/stdc++.h>
+
+typedef std::map<std::string, spbla_Matrix> MatrixMap;
+typedef std::unordered_set<std::string> SymbolSet;
+typedef std::pair<std::string, std::pair<std::string, std::string>> Rule;
+typedef std::vector<Rule> Rules;
+typedef unordered_map<string, pair<vector<spbla_Index>, vector<spbla_Index>>> Edges;
+
 #endif
