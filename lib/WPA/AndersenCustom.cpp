95603abe1c52ae676bdf11ec4afa74ae888b8ea7
Lukas Boettcher <1340215+lukasboettcher@users.noreply.github.com>
2022-06-22 04:05:29 +0200

    import storeEdge method from mmxcflr used to store an edge into edges vector for mmxcflr

--- lib/WPA/AndersenCustom.cpp
+++ lib/WPA/AndersenCustom.cpp
@@ -5,6 +5,12 @@ using namespace SVFUtil;
 
 AndersenCustom *AndersenCustom::customInstance = nullptr;
 
+void AndersenCustom::storeEdge(Edges &edges, NodeID src, NodeID dst, s64_t consEdgeType)
+{
+    string type = to_string(consEdgeType);
+    edges[type].first.push_back((spbla_Index)src);
+    edges[type].second.push_back((spbla_Index)dst);
+};
 void AndersenCustom::solveWorklist()
 {
     // while (!isWorklistEmpty())
