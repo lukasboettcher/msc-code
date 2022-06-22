9ea8b8c34b3a45e84fd48ed1fc58aabcdadaa192
Lukas Boettcher <1340215+lukasboettcher@users.noreply.github.com>
2022-06-22 04:06:46 +0200

    added fillEdges method use consg to store all edges into Edges object

--- lib/WPA/AndersenCustom.cpp
+++ lib/WPA/AndersenCustom.cpp
@@ -11,6 +11,32 @@ void AndersenCustom::storeEdge(Edges &edges, NodeID src, NodeID dst, s64_t consE
     edges[type].first.push_back((spbla_Index)src);
     edges[type].second.push_back((spbla_Index)dst);
 };
+void AndersenCustom::fillEdges(Edges &edges)
+{
+    for (auto &entry : *consCG)
+    {
+        for (auto &x : entry.second->getOutEdges())
+        {
+            if (const GepCGEdge *gepEdge = SVFUtil::dyn_cast<GepCGEdge>(x))
+            {
+                spbla_vec_t pts;
+                for (auto &addr_in : entry.second->getAddrInEdges())
+                    pts.push_back(addr_in->getSrcID());
+
+                unordered_set<SVF::NodeID> pts_in = processGepPts(edges, consCG, pag, pts, gepEdge);
+                for (auto &node : pts_in)
+                {
+                    storeEdge(edges, node, gepEdge->getDstID(), 0);
+                }
+            }
+            else
+            {
+                storeEdge(edges, x->getSrcID(), x->getDstID(), x->getEdgeKind());
+            }
+        }
+    }
+}
+
 void AndersenCustom::solveWorklist()
 {
     // while (!isWorklistEmpty())
@@ -21,4 +47,9 @@ void AndersenCustom::solveWorklist()
     //     processNode(nodeId);
     //     collapseFields();
     // }
+    PointsToMap ptsMap, copyMap;
+    Edges edges;
+    fillEdges(edges);
+    string grammar_file = "/home/lukas/Documents/msc-test/graspan_rules/custom/pointsto-streamlines.txt";
+
 }
