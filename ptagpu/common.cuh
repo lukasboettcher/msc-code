afebaf868deda945bb4a17a9580364b413f7922b
Lukas Boettcher <1340215+lukasboettcher@users.noreply.github.com>
2022-07-09 23:41:59 +0200

    added checkCuda to common header used to check return value after running cuda library calls

--- ptagpu/common.cuh
+++ ptagpu/common.cuh
@@ -1,6 +1,20 @@
 #ifndef COMMON_HEADER
 #define COMMON_HEADER
 
+#include <cstdio>
+#include <cuda.h>
+#include <cuda_runtime_api.h>
 
+#define checkCuda(val) check((val), #val, __FILE__, __LINE__)
+void check(cudaError_t err, const char *const func, const char *const file, const int line)
+{
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "CUDA Runtime Error at: '%s':%i\n\t%s %s\n", __FILE__, __LINE__, cudaGetErrorString(err), func);
+        exit(EXIT_FAILURE);
+    }
+}
+
+int run();
 
 #endif
