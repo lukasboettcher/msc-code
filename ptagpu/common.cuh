b35bbdcff8e261536a46d9a6a348f7679493385e
Lukas Boettcher <1340215+lukasboettcher@users.noreply.github.com>
2022-07-11 01:15:07 +0200

    copied good default values for the element structure from mendez et al

--- ptagpu/common.cuh
+++ ptagpu/common.cuh
@@ -1,6 +1,22 @@
 #ifndef COMMON_HEADER
 #define COMMON_HEADER
 
+#define BASE (30U)
+#define NEXT (31U)
+#define ELEMENT_WIDTH 32
+#define ELEMENT_CARDINALITY (30 * 32)
+#define BASE_OF(x) ((x) / ELEMENT_CARDINALITY)
+#define WORD_OF(x) (div32((x) % ELEMENT_CARDINALITY))
+#define BIT_OF(x) (mod32(x))
+
+__device__ __host__ static inline uint div32(uint num) {
+  return num >> 5;
+}
+
+__device__ __host__ static inline uint mod32(uint num) {
+  return num & 31;
+}
+
 #include <cstdio>
 #include <cuda.h>
 #include <cuda_runtime_api.h>
