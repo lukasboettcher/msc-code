68a99b1101bfe634a97f47ac2b3d2d72ebceadcf
Lukas Boettcher <1340215+lukasboettcher@users.noreply.github.com>
2022-08-07 04:31:47 +0200

    update test.c w/ a simple test case to test the ptagpu implementation on known bitcode

--- src/test.c
+++ src/test.c
@@ -1,36 +1,9 @@
-// #include "aliascheck.h"
-// #include "stdlib.h"
+void MAYALIAS(void *p, void *q);
 
-void MAYALIAS(void *p, void *q)
+int main()
 {
-	printf("\n");
-}
-
-// void MUSTALIAS(void *p, void *q)
-// {
-// 	printf("\n");
-// }
-
-// void NOALIAS(void *p, void *q)
-// {
-// 	printf("\n");
-// }
-
-struct MyStruct {
-	int * f1;
-	struct MyStruct * next;
-};
-
-int main() {
-	struct MyStruct * p = (struct MyStruct *) malloc (sizeof(struct MyStruct));
-	int num = 10;
-	while (num) {
-		p->next = (struct MyStruct *) malloc (sizeof(struct MyStruct));
-		p->next->f1 = (int *) malloc (sizeof(int));
-		p = p->next;
-	}
-	struct MyStruct *q = p;
-	MAYALIAS(q->next, p->next->next);
-	MAYALIAS(q->f1, p->next->f1);
-	return 0;
+	int a, b, *c, *d;
+	c = &a;
+	d = &a;
+	MAYALIAS(c, d);
 }
