2a9bf86d69b82ccd624d44adbf508d0b12019065
Lukas Boettcher <1340215+lukasboettcher@users.noreply.github.com>
2022-08-15 03:00:34 +0200

    simplify insertBitvector method handle the copy resolution in a special device function for copy rewrite rules

--- ptagpu/common.cu
+++ ptagpu/common.cu
@@ -181,9 +181,7 @@ __host__ void insertEdge(uint src, uint dst, uint *graph)
     }
 }
 
-__device__ void collectBitvectorTargets(const uint index, const uint bits, const uint base, uint *storage, uint &usedStorage, uint *originMemory, uint *targetMemory, const uint toRel);
-
-__device__ void insertBitvector(const uint index, const uint *originMemory, uint *targetMemory, uint *storage, uint &usedStorage, uint toIndex, uint fromBits, uint toRel)
+__device__ void insertBitvector(const uint *originMemory, uint *targetMemory, uint toIndex, uint fromBits, uint toRel)
 {
     while (1)
     {
@@ -196,12 +194,6 @@ __device__ void insertBitvector(const uint index, const uint *originMemory, uint
         uint val = threadIdx.x == NEXT ? toNext : fromBits;
         // write new values to target memory location
         targetMemory[toIndex + threadIdx.x] = val;
-
-        if (toRel == COPY)
-        {
-            uint fromBase = __shfl_sync(0xFFFFFFFF, fromBits, 30);
-            collectBitvectorTargets(index, fromBits, fromBase, storage + 128, usedStorage, __pts__, __ptsNext__, PTS_NEXT);
-        }
 
         // exit if no more elements in from bitvector
         if (fromNext == UINT_MAX)
@@ -638,8 +630,7 @@ __host__ bool alias(uint a, uint b, uint *memory)
 
 __device__ void cloneAndLink(uint var, const uint ptsIndex, uint &currDiffPtsIndex, const uint diffPtsBits, const uint diffPtsNext, uint *pts, uint *curr_pts, uint *next_pts)
 {
-    // clone(ptsIndex, diffPtsBits, diffPtsNext, PTS);
-    insertBitvector(0, next_pts, pts, nullptr, var, ptsIndex, diffPtsBits, PTS);
+    insertBitvector(next_pts, pts, ptsIndex, diffPtsBits, PTS);
     if (currDiffPtsIndex != UINT_MAX)
     {
         curr_pts[currDiffPtsIndex + NEXT] = ptsIndex;
