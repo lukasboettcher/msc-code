b095e9391543e2000243321c9065c6530e8186a4
Lukas Boettcher <1340215+lukasboettcher@users.noreply.github.com>
2022-08-22 20:25:07 +0200

    reorder bitvector indices

--- ptagpu/common.cuh
+++ ptagpu/common.cuh
@@ -13,13 +13,13 @@
 #define N_BLOCKS 16
 
 #define PTS 0
-#define COPY 1
-#define STORE 2
-#define LOAD 3
-#define GEP 4
-#define PTS_CURR 5
-#define PTS_NEXT 6
-#define N_TYPES 7
+#define PTS_CURR 1
+#define PTS_NEXT 2
+#define COPY 3
+#define LOAD 4
+#define STORE 5
+#define N_TYPES 6
+
 
 __device__ __host__ static inline uint div32(uint num)
 {
