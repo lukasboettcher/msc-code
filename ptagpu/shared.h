8f452802f991d74ef6a85cfe43a58f695fceb666
Lukas Boettcher <1340215+lukasboettcher@users.noreply.github.com>
2022-08-22 20:22:39 +0200

    add the shared header to svfhooks since edgeSets are also used here

--- ptagpu/shared.h
+++ ptagpu/shared.h
@@ -7,6 +7,6 @@ typedef std::pair<std::vector<unsigned int>, std::vector<unsigned int>> edgeSet;
 typedef std::pair<std::pair<std::vector<unsigned int>, std::vector<unsigned int>>, std::vector<unsigned int>> edgeSetOffset;
 
 unsigned int *run(unsigned int numNodes, edgeSet *addr, edgeSet *direct, edgeSet *load, edgeSet *store, void *consG, void *pag);
-bool aliasBV(uint a, uint b, uint *memory);
+bool aliasBV(unsigned int a, unsigned int b, unsigned int *memory);
 
 #endif
--- ptagpu/svfhook.h
+++ ptagpu/svfhook.h
@@ -1,9 +1,8 @@
 #ifndef SVFHOOK_HEADER
 #define SVFHOOK_HEADER
 
-#include <vector>
+#include "shared.h"
 
-typedef std::pair<std::vector<unsigned int>, std::vector<unsigned int>> edgeSet;
 unsigned int handleGep(void *consG, void *pag, unsigned int id, unsigned int offset);
 unsigned int getNodeCount(void *consG);
 void handleGepsSVF(void *cg, void *pg, unsigned int *currPts, edgeSet &newPtsEdges);
